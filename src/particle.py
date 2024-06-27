92a249402760238a42716f9248d6147a33a8b330
Eric2422 <ericbkzhang@gmail.com>
2024-06-26 21:15:15 -0400

    Implement the Biot-Savart Law for point charges (30 min)

--- src/particle.py
+++ src/particle.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import math
 
 import numpy as np
-from scipy import constants
+import scipy.constants
 
 
 class Particle:
@@ -57,21 +57,48 @@ class Particle:
         """
         distance = math.dist(list(self.position), list(particle.position))
 
-        k = 1 / (4 * constants.pi * constants.epsilon_0)
+        k = 1 / (4 * scipy.constants.pi * scipy.constants.epsilon_0)
 
         # The magnitude of the electric force.
         force_magnitude = (k * self.charge * particle.charge) / (distance ** 2)
 
-        vectorBetweenPoints = particle.position - self.position
-        vectorMagnitude = np.linalg.norm(distance)
+        vector_between_particles = particle.position - self.position
 
         # Break the electric force into X, Y, and Z components
         force_vector = np.array(
-            [-force_magnitude * (coordinate / vectorMagnitude)
-             for coordinate in vectorBetweenPoints]
+            [-force_magnitude * (coordinate / distance)
+             for coordinate in vector_between_particles]
         )
 
         return force_vector
+
+    def biot_savart_law(self, particle: Particle) -> np.array:
+        """Calculate the magnetic force exerted on this particle by another particle. 
+        It uses the "Biot-Savart Law for point charges," technically a misnomer,
+        which only approximates magnetic fields for particles with a velocity << c.
+
+        Parameters
+        ----------
+        particle : Particle
+            Another particle which is exerting a magnetic field on this particle
+
+        Returns
+        -------
+        np.array
+            A vector the magnetic force that this particle experiences due to `particle`
+        """
+        # The vector between the positions of the particles
+        r = particle.position - self.position
+        r_hat = r / np.linalg.norm(r)
+
+        magnetic_field = (scipy.constants.mu_0 * particle.charge * np.cross(particle.velocity * r_hat) 
+                          / (4 * np.pi * r ** 2))
+        
+        print(f'B field: {magnetic_field}')
+
+        magnetic_force = self.charge * np.cross(self.velocity, magnetic_field)
+
+        return magnetic_force
 
     def __str__(self) -> str:
         coordinates = f'({", ".join([str(num) for num in self.position])})'
