3c87965f264eda9990bacd8754a669ac57885abf
Eric Zhang <ericbkzhang@gmail.com>
2024-11-21 17:45:46 -0500

    Adding pandas

--- src/plot.py
+++ src/plot.py
@@ -1,10 +1,11 @@
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 class Plot():
-    def __init__(self, data: np.ndarray, tick_size: np.float64 = 1.0) -> None:
+    def __init__(self, data: list[pd.DataFrame], tick_size: np.float64 = 1.0) -> None:
         """Create a 3D NumPy plot.
 
         Parameters
@@ -19,24 +20,19 @@ class Plot():
         self.figure = plt.figure()
         self.ax = self.figure.add_subplot(111, projection='3d')
 
-        # print(f'data: {data[0]}')
-        # print()
-        # Plot 2D lines, one for each particle.
-        x_values = [particle[0][0] for particle in data]
-        y_values = [particle[0][1] for particle in data]
-        z_values = [particle[0][2] for particle in data]
+        self.data = data
+        print(self.data[:, 0, 0])
+        print()
+
+        # Plot scatter points, one for each particle.
+        x_values = self.data[:, 0, 0]
+        y_values = self.data[:, 0, 1]
+        z_values = self.data[:, 0, 2]
         self.scatter = self.ax.scatter(
             x_values,
             y_values,
             z_values
         )
-        print(data)
-
-        # print(points)
-
-        # for particle in data:
-        #     print(particle)
-        #     print()
 
         self.ax.margins(1, 1, 1)
 
@@ -44,23 +40,27 @@ class Plot():
         self.plot_animation = animation.FuncAnimation(
             self.figure,
             self.update,
-            interval=tick_size / 1000, # Convert from seconds to milliseconds.
-            fargs=(data)
+            interval=tick_size / 1000,  # Convert from seconds to milliseconds.
+            blit=True
         )
 
-    def update(self, num: int, data):
+    def update(self, num: int):
         """Update the plot points of the scatter. 
 
         Parameters
         ----------
         num : int
             The number of intervals that have elapsed.
-        data : np.ndarray
-            The position of the particles in the simulation.
         """
-        x_values = [particle[num][0] for particle in data]
-        y_values = [particle[num][1] for particle in data]
-        z_values = [particle[num][2] for particle in data]
+        print(num)
+        if num > len(self.data[0] - 2):
+            return
+
+        # print(self.data[:, num, 0])
+        # print('\n\n')
+        x_values = self.data[:, num, 0]
+        y_values = self.data[:, num, 1]
+        z_values = self.data[:, num, 2]
         self.scatter.set_offsets([x_values, y_values, z_values])
 
         return self.scatter,
