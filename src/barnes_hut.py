892de673994cabd65551ecbd65c8df29ee58b0e6
Eric Zhang <ericbkzhang@gmail.com>
2025-03-12 19:48:40 -0400

    Add param for adding particles to BH cells

--- src/barnes_hut.py
+++ src/barnes_hut.py
@@ -4,13 +4,16 @@ from particle import PointParticle
 import vectors
 
 class BarnesHutCell():
-    def __init__(self, x_bounds: np.ndarray[np.float64], y_bounds: np.ndarray[np.float64], z_bounds: np.ndarray[np.float64]):
+    def __init__(self, x_bounds: np.ndarray[np.float64], y_bounds: np.ndarray[np.float64], z_bounds: np.ndarray[np.float64], parent_particles: list = [PointParticle]):
         self.x_bounds = x_bounds
         self.y_bounds = y_bounds
         self.z_bounds = z_bounds
 
         self.children_particles = []
         self.children_cells = []
+
+    def create_child_cells(self):
+        pass
 
     def getCenterofMass(self) -> vectors.PositionVector:
         moments = np.zeros(shape=(3))
