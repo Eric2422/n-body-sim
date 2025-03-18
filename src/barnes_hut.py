768f090bb51ec9837910cc3fe45f4c384168d5fd
Eric Zhang <ericbkzhang@gmail.com>
2025-03-18 15:18:09 -0400

    Create child cells recursively

--- src/barnes_hut.py
+++ src/barnes_hut.py
@@ -24,16 +24,21 @@ class BarnesHutCell():
                 and particle.position[2] > z_bounds[0] and particle.position[2] < z_bounds[1]
         ]
 
-        if len(self.particles <= 1): 
-            self.child_cells = []
-        
-        else:
-            self.child_cells = [
-                
-            ]
+        cell_width = self.x_bounds[1] - self.x_bounds[0]
+        self.child_cells = [] if len(self.particles <= 1) else [
+            BarnesHutCell
+            for i in range(2):
+                for j in range(2):
+                    for k in range(2):
+                        pass
+        ]
 
     def create_child_cells(self):
-        pass
+        if len(self.particles <= 1):
+            return []
+        
+        width = self.x_bounds[1] - self.x_bounds[0]
+        return [BarnesHutCell() for i]
 
     def getCenterofMass(self) -> vectors.PositionVector:
         moments = np.zeros(shape=(3))
