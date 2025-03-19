efedc22a2d9db0917e0417eb59bb3c5fcdbdbebd
Eric Zhang <ericbkzhang@gmail.com>
2025-03-19 13:49:04 -0400

    Implementing recursive child cells

--- src/barnes_hut.py
+++ src/barnes_hut.py
@@ -10,42 +10,78 @@ class BarnesHutCell():
             x_bounds: np.ndarray[np.float64],
             y_bounds: np.ndarray[np.float64],
             z_bounds: np.ndarray[np.float64],
-            parent_particles: list = [PointParticle]
+            parent_particles: list[PointParticle] = []
     ):
         self.x_bounds = x_bounds
         self.y_bounds = y_bounds
         self.z_bounds = z_bounds
+        print(f'x: {self.x_bounds}')
+        print(f'y: {self.y_bounds}')
+        print(f'z: {self.z_bounds}')
 
         # Search through parent cell for all particles that are within this child cell
         self.particles = [
             particle for particle in parent_particles
-            if particle.position[0] > x_bounds[0] and particle.position[0] < x_bounds[1]
-                and particle.position[1] > y_bounds[0] and particle.position[1] < y_bounds[1]
-                and particle.position[2] > z_bounds[0] and particle.position[2] < z_bounds[1]
+            if particle.position[0] >= x_bounds[0] and particle.position[0] <= x_bounds[1]
+                and particle.position[1] >= y_bounds[0] and particle.position[1] <= y_bounds[1]
+                and particle.position[2] >= z_bounds[0] and particle.position[2] <= z_bounds[1]
         ]
 
-        cell_width = self.x_bounds[1] - self.x_bounds[0]
-        self.child_cells = [] if len(self.particles <= 1) else [
-            BarnesHutCell
-            for i in range(2):
-                for j in range(2):
-                    for k in range(2):
-                        pass
-        ]
+        self.child_cells = self.create_child_cells()
 
     def create_child_cells(self):
-        if len(self.particles <= 1):
-            return []
-        
         width = self.x_bounds[1] - self.x_bounds[0]
-        return [BarnesHutCell() for i]
 
-    def getCenterofMass(self) -> vectors.PositionVector:
+        child_cells = []
+
+        if len(self.particles) <= 1:
+            return child_cells
+
+        for x in range(2):
+            for y in range(2):
+                for z in range(2):
+                    child_cells.append(
+                        BarnesHutCell(
+                            np.array((self.x_bounds[0] + (x * width), self.x_bounds[0] + (2 * x * width))),
+                            np.array((self.y_bounds[0] + (y * width), self.y_bounds[0] + (2 * y * width))),
+                            np.array((self.z_bounds[0] + (z * width), self.z_bounds[0] + (2 * z * width)))
+                        )
+                    )
+        
+        return child_cells
+
+    def get_total_mass(self):
+        pass
+
+    def get_center_of_mass(self) -> vectors.PositionVector:
+        """Return the center of mass of this Barnes-Hut cell.
+
+        Returns
+        -------
+        vectors.PositionVector
+            A 3D NumPy array that represents a location.
+        """
         moments = np.zeros(shape=(3))
         total_mass = 0
+        
+        # If there are child cells, loop through them.
+        if len(self.child_cells > 0):
+            for child_cell in self.child_cells:
+                child_cell_mass = child_cell.get_total_mass()
+                moments += (child_cell_mass * particle.get_center_of_mass())
+                total_mass += child_cell_mass
 
-        for particle in self.children_particles:
-            moments += (particle.mass * particle.position)
-            total_mass += particle.mass
+        # If there are no child cells, loop through the particles of this cell.
+        else:
+            for particle in self.particles:
+                moments += (particle.mass * particle.position)
+                total_mass += particle.mass
 
         return moments / total_mass
+
+    def __str__(self):
+        return f'''({self.x_bounds[0]}, {self.x_bounds[1]})
+({self.y_bounds[0]}, {self.y_bounds[1]})
+({self.z_bounds[0]}, {self.z_bounds[1]})
+{len(self.child_cells)} child cell(s)
+{len(self.particles)} particle(s)'''
