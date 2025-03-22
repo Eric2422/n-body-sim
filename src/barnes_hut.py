e56ffa61d04141d891eb62ba7f26f39de6c95171
Eric Zhang <ericbkzhang@gmail.com>
2025-03-22 14:04:22 -0400

    Add total mass to BH cell

--- src/barnes_hut.py
+++ src/barnes_hut.py
@@ -20,8 +20,8 @@ class BarnesHutCell():
         self.particles = [
             particle for particle in parent_particles
             if particle.position[0] >= x_bounds[0] and particle.position[0] <= x_bounds[1]
-                and particle.position[1] >= y_bounds[0] and particle.position[1] <= y_bounds[1]
-                and particle.position[2] >= z_bounds[0] and particle.position[2] <= z_bounds[1]
+            and particle.position[1] >= y_bounds[0] and particle.position[1] <= y_bounds[1]
+            and particle.position[2] >= z_bounds[0] and particle.position[2] <= z_bounds[1]
         ]
 
         self.child_cells = self.create_child_cells()
@@ -48,12 +48,24 @@ class BarnesHutCell():
                             np.array((lower_z, lower_z + width))
                         )
                     )
-        
+
         return child_cells
 
     def get_total_mass(self):
-        if len(self.child_cells > 1):
-            pass
+        total_mass = 0
+
+        # If there are child cells, loop through them to sum the moments and masses.
+        if len(self.child_cells) > 0:
+            for child_cell in self.child_cells:
+                total_mass += child_cell.get_total_mass()
+
+        # If there are no child cells, loop through the particles of this cell.
+        else:
+            for particle in self.particles:
+                total_mass += particle.mass
+
+        return total_mass
+            
 
     def get_center_of_mass(self) -> vectors.PositionVector:
         """Return the center of mass of this Barnes-Hut cell.
@@ -65,13 +77,11 @@ class BarnesHutCell():
         """
         moments = np.zeros(shape=(3))
         total_mass = 0
-        
-        # If there are child cells, loop through them.
+
+        # If there are child cells, loop through them to sum the moments and masses.
         if len(self.child_cells) > 0:
             for child_cell in self.child_cells:
-                print(child_cell)
                 child_cell_mass = child_cell.get_total_mass()
-                print(child_cell_mass)
                 moments += (child_cell_mass * child_cell.get_center_of_mass())
                 total_mass += child_cell_mass
 
@@ -81,7 +91,9 @@ class BarnesHutCell():
                 moments += (particle.mass * particle.position)
                 total_mass += particle.mass
 
-        return moments / total_mass
+        # If mass is not zero, return the center of mass.
+        # Else return the centroid
+        return moments / total_mass if total_mass != 0 else np.array(((np.mean(self.x_bounds), np.mean(self.y_bounds), np.mean(self.z_bounds))))
 
     def __str__(self):
         return f'''X: [{self.x_bounds[0]}, {self.x_bounds[1]}], Y: [{self.y_bounds[0]}, {self.y_bounds[1]}], Z: [{self.z_bounds[0]}, {self.z_bounds[1]}]
