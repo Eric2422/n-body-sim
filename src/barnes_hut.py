3fbce4c615f1837f0e42b35eb60e4b49bc2a8db8
Eric Zhang <ericbkzhang@gmail.com>
2025-04-01 12:05:34 -0400

    Fixed the recurive center of mass for BH cells

--- src/barnes_hut.py
+++ src/barnes_hut.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 from particle import PointParticle
 import vectors
@@ -12,102 +13,147 @@ class BarnesHutCell():
             z_bounds: np.ndarray[np.float64],
             particles: list[PointParticle] = []
     ):
+        """Constructs a Barnes-Hut cell and recursively create its child nodes. 
+
+        Parameters
+        ----------
+        x_bounds : np.ndarray[np.float64]
+            A 2-element NumPy array that contains the lower and upper X bounds, in that order
+        y_bounds : np.ndarray[np.float64]
+            A 2-element NumPy array that contains the lower and upper Y bounds, in that order
+        z_bounds : np.ndarray[np.float64]
+            A 2-element NumPy array that contains the lower and upper Z bounds, in that order
+        particles : list[PointParticle], optional
+            List of particles that are contained within this Barnes-Hut cell.
+            Assumed to not contain any particles out of the bounds, by default []
+        """
+
         self.x_bounds = x_bounds
         self.y_bounds = y_bounds
         self.z_bounds = z_bounds
 
-        self.mass = 0
-        self.center_of_mass = np.empty(3)
+        self.total_mass = 0
 
-        # If there is more than one particle
+        total_moment = np.zeros(3)
+
+        # If this cell is an internal node(i.e. it has more than 1 particle)
         if len(particles) > 1:
-            for particle in particles:
-                if particle.position:
-                    pass
+            self.child_cells = self.create_child_cells(particles)
+
+            for child_cell in self.child_cells:
+                self.total_mass += child_cell.total_mass
+                total_moment += child_cell.total_mass * child_cell.center_of_mass
 
         # If this is an external node(i.e. it has only 0 or 1 particles)
         else:
+            self.child_cells = []
+
             for particle in particles:
-                self.mass += particle.mass
-                self.center_of_mass += particle.mass * particle.position
+                self.total_mass += particle.mass
+                total_moment += particle.mass * particle.position
 
-            self.center_of_mass /= self.mass if self.mass != 0 else np.array(
-                np.mean(x_bounds), np.mean(y_bounds), np.mean(z_bounds))
+        # Divide the total moment by center of mass to obtain the center of mass
+        # If mass is 0, return the centroid
+        self.center_of_mass = total_moment / self.total_mass if self.total_mass != 0 else np.array((
+            np.mean(x_bounds), np.mean(y_bounds), np.mean(z_bounds)))
 
-        self.child_cells = self.create_child_cells()
+    def create_child_cells(self, particles):
+        centroid = np.array((
+            np.mean(self.x_bounds),
+            np.mean(self.y_bounds),
+            np.mean(self.z_bounds)
+        ))
 
-    def create_child_cells(self):
-        width = self.x_bounds[1] - self.x_bounds[0]
+        # Width of child cells
+        child_width = self.x_bounds[1] - self.x_bounds[0] / 2
 
+        # List of all the child BH cells
         child_cells = []
 
-        if len(self.particles) <= 1:
-            return child_cells
+        # Stores the particles in each octant
+        # Indexed by x(left-right), y(back-front), z(bottom-top)
+        octant_particles_list = (
+            (([], []),
+             ([], [])),
+            (([], []),
+             ([], []))
+        )
 
-        for x in range(2):
-            for y in range(2):
-                for z in range(2):
-                    lower_x = self.x_bounds[0] + (x * width)
-                    lower_y = self.y_bounds[0] + (y * width)
-                    lower_z = self.z_bounds[0] + (z * width)
+        # Loop through each particle and categorize them into an octant
+        for particle in particles:
+            # Right-front-top
+            if particle.position[0] >= centroid[0] and particle.position[1] >= centroid[1] and particle.position[2] >= centroid[2]:
+                octant_particles_list[1][1][1].append(particle)
+
+            # Left-front-top
+            elif particle.position[0] <= centroid[0] and particle.position[1] >= centroid[1] and particle.position[2] >= centroid[2]:
+                octant_particles_list[0][1][1].append(particle)
+
+            # Right-back-top
+            elif particle.position[0] >= centroid[0] and particle.position[1] <= centroid[1] and particle.position[2] >= centroid[2]:
+                octant_particles_list[1][0][1].append(particle)
+
+            # Left-back-top
+            elif particle.position[0] <= centroid[0] and particle.position[1] <= centroid[1] and particle.position[2] >= centroid[2]:
+                octant_particles_list[0][0][1].append(particle)
+
+            # Right-front-bottom
+            elif particle.position[0] >= centroid[0] and particle.position[1] >= centroid[1] and particle.position[2] <= centroid[2]:
+                octant_particles_list[1][1][0].append(particle)
+
+            # Left-front-bottom
+            elif particle.position[0] <= centroid[0] and particle.position[1] >= centroid[1] and particle.position[2] <= centroid[2]:
+                octant_particles_list[0][1][0].append(particle)
+
+            # Right-back-bottom
+            elif particle.position[0] >= centroid[0] and particle.position[1] <= centroid[1] and particle.position[2] <= centroid[2]:
+                octant_particles_list[1][0][0].append(particle)
+
+            # Left-back-bottom
+            elif particle.position[0] <= centroid[0] and particle.position[1] <= centroid[1] and particle.position[2] <= centroid[2]:
+                octant_particles_list[0][0][0].append(particle)
+
+        # Loop eight times to create the octants
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                    lower_x = self.x_bounds + (i * child_width)
+                    lower_y = self.y_bounds + (j * child_width)
+                    lower_z = self.z_bounds + (k * child_width)
 
                     child_cells.append(
                         BarnesHutCell(
-                            np.array((lower_x, lower_x + width)),
-                            np.array((lower_y, lower_y + width)),
-                            np.array((lower_z, lower_z + width))
+                            particles=octant_particles_list[i][j][k],
+                            x_bounds=np.array(
+                                (lower_x, lower_x + child_width)),
+                            y_bounds=np.array(
+                                (lower_y, lower_y + child_width)),
+                            z_bounds=np.array((lower_z, lower_z + child_width))
                         )
                     )
 
         return child_cells
 
-    def get_total_mass(self):
-        total_mass = 0
+    def get_gravitationl_field(self, point: vectors.PositionVector) -> vectors.FieldVector:
+        """Calculate the approximate gravitational field exerted by this cell at a certain point.
 
-        # If there are child cells, loop through them to sum the moments and masses.
-        if len(self.child_cells) > 0:
-            for child_cell in self.child_cells:
-                total_mass += child_cell.get_total_mass()
-
-        # If there are no child cells, loop through the particles of this cell.
-        else:
-            for particle in self.particles:
-                total_mass += particle.mass
-
-        return total_mass
-
-    def get_center_of_mass(self) -> vectors.PositionVector:
-        """Return the center of mass of this Barnes-Hut cell.
+        Parameters
+        ----------
+        point : vectors.PositionVector
+            A 3D NumPy array representing a 3D position vector. Measured in meters(m).
 
         Returns
         -------
-        vectors.PositionVector
-            A 3D NumPy array that represents a location.
+        vectors.FieldVector
+            A 3D NumPy array representing a 3D field vector. Measured in newtons per kg (N / kg).
         """
-        moments = np.zeros(shape=(3))
-        total_mass = 0
+        vector_between_points = point - self.center_of_mass
+        distance = np.linalg.norm(vector_between_points)
+        unit_vector = vector_between_points / distance
 
-        # If there are child cells, loop through them to sum the moments and masses.
-        if len(self.child_cells) > 0:
-            for child_cell in self.child_cells:
-                child_cell_mass = child_cell.get_total_mass()
-                moments += (child_cell_mass * child_cell.get_center_of_mass())
-                total_mass += child_cell_mass
-
-        # If there are no child cells, loop through the particles of this cell.
-        else:
-            for particle in self.particles:
-                moments += (particle.mass * particle.position)
-                total_mass += particle.mass
-
-        # If mass is not zero, return the center of mass.
-        # Else return the centroid
-        return moments / total_mass if total_mass != 0 else np.array(((np.mean(self.x_bounds), np.mean(self.y_bounds), np.mean(self.z_bounds))))
-
-    def get_gravitationl_field(self, position: vectors.PositionVector) -> vectors.FieldVector:
-        pass
+        return unit_vector * scipy.constants.G * self.total_mass / distance ** 2
 
     def __str__(self):
-        return f'''X: [{self.x_bounds[0]}, {self.x_bounds[1]}], Y: [{self.y_bounds[0]}, {self.y_bounds[1]}], Z: [{self.z_bounds[0]}, {self.z_bounds[1]}]
-Center of Mass: {self.get_center_of_mass()}
-{len(self.child_cells)} child cell(s), {len(self.particles)} particle(s)'''
+        return f'''X: [{self.x_bounds[0]}, {self.x_bounds[1]}], Y: [{self.y_bounds[1]}, {self.y_bounds[1]}], Z: [{self.z_bounds[0]}, {self.z_bounds[1]}]
+Center of Mass: {self.center_of_mass}
+{len(self.child_cells)} child cell(s)'''
