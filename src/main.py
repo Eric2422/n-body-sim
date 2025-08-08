b988204818118a96cd5ee5fead269399bceebd0f
Eric Zhang <ericbkzhang@gmail.com>
2025-08-08 07:52:57 -0400

    Undo accidental inclusion of Barnes-Hut in the main branch

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 import pandas as pd
 
-from barnes_hut import BarnesHutCell
 from files import FileHandler
 from particle import PointParticle
 from plot import Plot
@@ -11,12 +10,8 @@ import vectors
 
 
 class Simulation():
-    """Represents one simulation with particles and fields.
-    """
-
     def __init__(
         self,
-        theta: float = 0.5,
         tick_size: float = 1.0,
         gravitational_field: vectors.FieldVector = np.zeros(3, dtype=float),
         electric_field: vectors.FieldVector = np.zeros(3, dtype=float),
@@ -27,8 +22,6 @@ class Simulation():
 
         Parameters
         ----------
-        theta : float, optional
-            The Barnes-Hut approximation parameter, by default 0.5
         tick_size : float, optional
             The time increment of the simulation in seconds, by default 1.0
         gravitational_field : vectors.FieldVector, optional
@@ -41,14 +34,13 @@ class Simulation():
             A list of particles that are interacting with each other., by default []
         """
         self.particles = particles
-
+        # A log of all the particles' positions over the course of the simulation
         self.particle_positions_log = pd.DataFrame({
-            't': np.empty(0, dtype=float),
-            'x': np.empty(0, dtype=float),
-            'y': np.empty(0, dtype=float),
-            'z': np.empty(0, dtype=float)
+            't': np.empty(0),
+            'x': np.empty(0),
+            'y': np.empty(0),
+            'z': np.empty(0)
         })
-        """A log of all the particles' positions over the course of the simulation."""
 
         # Constant, universal fields
         self.electric_field = electric_field
@@ -58,87 +50,59 @@ class Simulation():
         self.current_tick = 0.0
         self.tick_size = tick_size
 
-        self.theta = theta
-        """The Barnes-Hut approximation parameter."""
-
-    def create_barnes_hut_nodes(self) -> BarnesHutCell:
-        return BarnesHutCell(particles=self.particles)
-
-    def log_particle_position(self, particle: PointParticle) -> None:
-        """Save the position of a particle to the particle positions log.
+    def apply_force_between_particles(self, particle1: PointParticle, particle2: PointParticle) -> None:
+        """Calculate and apply the force from one particle upon another.
 
         Parameters
         ----------
-        particle : PositionParticle
-            A particle to save the position of.
+        particle1 : PointParticle
+            The particle that is being being moved by `particle2`.
+        particle2 : PointParticle
+            The particle that is exerting a force upon `particle1`.
         """
-        # Save particle position data
-        new_data = pd.DataFrame([{
-            't': self.current_tick * self.tick_size,
-            'x': np.array((particle.position[0])),
-            'y': np.array((particle.position[1])),
-            'z': np.array((particle.position[2]))
-        }])
-        self.particle_positions_log = pd.concat(
-            [self.particle_positions_log, new_data], ignore_index=True)
-
-    def get_particle_positions_string(self) -> str:
-        """Return a string of the particles' current state.
-
-        Returns
-        -------
-        str
-            A string describing the state of all particles.
-        """
-
-        # Add the initial time and particle data to the file
-        output_string = f't={self.current_tick * self.tick_size}\n'
-
-        for particle in self.particles:
-            output_string += particle.__str__() + '\n'
-
-        output_string += '\n'
-
-        return output_string
+        # If the particles are not the same
+        if particle1 != particle2:
+            # Lorentz force law
+            particle1.apply_fields(
+                particle2.get_gravitational_field_exerted(particle1.position),
+                particle2.get_electric_field_exerted(particle1.position),
+                particle2.get_magnetic_field_exerted(particle1.position)
+            )
 
     def tick(self) -> None:
         """Run one tick of the simulation."""
-        # Get the root node of the octree
-        barnes_hut_root = self.create_barnes_hut_nodes()
-        print(barnes_hut_root)
-
-        # An array of net force acting upon each particle
-        net_forces = np.zeros(shape=(len(self.particles), 3))
-
-        for particle in self.particles:
-            particle.set_force()
-
         # Calculate the forces that the particles exert on each other
         # Update the particle's acceleration and, but not the velocity and position
+        index = 0
+
         for i in range(len(self.particles)):
             particle1 = self.particles[i]
 
-            for child_node in barnes_hut_root.child_cells:
-                net_forces[i] += particle1.get_gravitational_force_experienced(
-                    child_node.get_gravitational_field_exerted(particle1.position))
-
-                net_forces[i] += particle1.get_electrostatic_force_experienced(
-                    child_node.get_electric_field_exerted(particle1.position)
-                )
-
-                net_forces[i] += particle1.get_magnetic_force_experienced(
-                    child_node.get_magnetic_field_exerted(particle1.position)
-                )
+            for j in range(i + 1, len(self.particles)):
+                # print(f'i, j: {i}, {j}')
+                particle2 = self.particles[j]
+                self.apply_force_between_particles(particle1, particle2)
 
             # Add the constant fields
-            net_forces[i] += particle1.get_force_experienced(
-                self.electric_field, self.magnetic_field, self.gravitational_field
+            particle1.apply_fields(
+                self.gravitational_field, self.electric_field, self.magnetic_field
             )
 
         # Update particle positions and velocities after calculating the forces,
         # so it doesn't affect force calculations.
+        index = 0
         for particle in particles:
-            self.log_particle_position(particle)
+            # Save particle position data
+            new_data = pd.DataFrame([{
+                't': self.current_tick * self.tick_size,
+                'x': np.array((particle.position[0])),
+                'y': np.array((particle.position[1])),
+                'z': np.array((particle.position[2]))
+            }])
+            self.particle_positions_log = pd.concat(
+                [self.particle_positions_log, new_data], ignore_index=True)
+
+            index += 1
 
             # Update the particle's velocity
             particle.velocity += particle.acceleration * self.tick_size
@@ -157,28 +121,22 @@ class Simulation():
             The number of ticks that the simulation runs by, by default 1
         file_handler : FileHandler, optional
             A `FileHandler` object to pass data into as the simulation runs.
-            Writes the data into a file,
+            Writes the data into a file, 
             so the data does not need to be looped through again afterward.
             By default None
         print_progress : bool, optional
             Whether to print a progress report on how much of the simulation has been completed, by default False
         """
-        output_string = ''
+
+        # By default, run the entire simulation
+        if num_ticks == None:
+            num_ticks = self.total_ticks
+
         if file_handler is not None:
             file_handler.clear_output_file()
 
-            # Print fields
-            output_string += f'g=<{", ".join((str(dimension) for dimension in self.gravitational_field))}>\n'
-            output_string += f'E=<{", ".join((str(dimension) for dimension in self.electric_field))}>\n'
-            output_string += f'B=<{", ".join((str(dimension) for dimension in self.magnetic_field))}>\n\n'
-
-            output_string += self.get_particle_positions_string()
-
-        # Record initial particle data
-        for particle in particles:
-            self.log_particle_position(particle=particle)
-
         # Run the necessary number of ticks
+        output_string = ''
         for i in range(num_ticks):
             self.tick()
             progress = i / num_ticks if num_ticks == 0 else float(1.0)
@@ -191,7 +149,14 @@ class Simulation():
 
             # If a `FileHandler` object is passed, output the results to a file.
             if file_handler is not None:
-                output_string += self.get_particle_positions_string()
+                # Add the current time and particle data to the file
+                output_string += f'''Time: {self.current_tick *
+                                            self.tick_size} s\n'''
+
+                for particle in self.particles:
+                    output_string += particle.__str__() + '\n'
+
+                output_string += '\n'
 
         if file_handler is not None:
             file_handler.append_to_output_file(output_string)
@@ -224,25 +189,17 @@ if __name__ == '__main__':
 
     # Create and run the simulation
     simulation = Simulation(
-        theta=file_data['theta'],
         tick_size=file_data['tick size'],
         gravitational_field=file_data['gravitational field'],
         electric_field=file_data['electric field'],
         magnetic_field=file_data['magnetic field'],
         particles=particles
     )
-
-    simulation.run(
-        num_ticks=file_data['num ticks'],
-        file_handler=file_handler,
-        print_progress=True
-    )
+    simulation.run(file_handler=file_handler, print_progress=True)
 
     # Plot the simulation
     plot = Plot(
         data_frame=simulation.particle_positions_log,
         tick_size=simulation.tick_size
     )
-
-    # plot.save_plot_to_file()
-    # plot.show()
+    plot.show()
