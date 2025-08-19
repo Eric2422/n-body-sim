d5e6713850315660b42e39bf31c0a9809e13de2c
Eric Zhang <ericbkzhang@gmail.com>
2025-08-19 10:35:15 -0400

    Change `bh_test.py` to run from a JSON file rather than hardcoded values.
    This prevents the need to create commits everytime the test values are changed.

--- src/bh_test.py
+++ src/bh_test.py
@@ -1,23 +1,31 @@
 import numpy as np
+import sys
 
 from barnes_hut import BarnesHutCell
+import files
 from particle import PointParticle
 
+# Check if the user supplied a input file
+if len(sys.argv) < 2:
+    raise ValueError('Please enter the name of the input file.')
+
+# Read the input file data and create particles based on that data
+file_handler = files.FileHandler(input_file=sys.argv[1])
+file_data = file_handler.read_input_file()
+
+# Create a list of particles as described by the file data.
 particles = [
-    PointParticle(np.array((0, 0, 0))),
-    PointParticle(np.array((0, 100, 0))),
-    PointParticle(np.array((100, 100, 0))),
-    PointParticle(np.array((100, 0, 0))),
-    PointParticle(np.array((0, 0, 100))),
-    PointParticle(np.array((0, 100, 100))),
-    PointParticle(np.array((100, 100, 100))),
-    PointParticle(np.array((100, 0, 100)))
+    PointParticle(
+        position=np.array(particle['position']),
+        velocity=np.array(particle['velocity']),
+        acceleration=np.array(particle['acceleration']),
+        mass=particle['mass'],
+        charge=particle['charge']
+    )
+    for particle in file_data['particles']
 ]
 
 barnes_hut_cell = BarnesHutCell(
-    np.array((0, 100)),
-    np.array((0, 100)),
-    np.array((0, 100)),
     particles=particles
 )
 
