a219ebd26439eeee9e1230190f2fa16685ce9b4a
Eric Zhang <ericbkzhang@gmail.com>
2025-12-21 20:18:24 -0500

    Add comments for plot

--- src/plot.py
+++ src/plot.py
@@ -37,9 +37,10 @@ class Plot():
             marker="o"
         )
 
-        # Set dimensions of plot.
         # Scalar margin
         MARGIN = 1.25
+
+        # Set x limits.
         min_x = np.min(np.array(data_frame['x'].values))
         max_x = np.max(np.array(data_frame['x'].values))
         width = max_x - min_x
@@ -49,6 +50,7 @@ class Plot():
         )
         ax.set_xlabel('x (m)')
 
+        # Set y limits.
         min_y = np.min(np.array(data_frame['y'].values))
         max_y = np.max(np.array(data_frame['y'].values))
         length = max_y - min_y
@@ -58,6 +60,7 @@ class Plot():
         )
         ax.set_ylabel('y (m)')
 
+        # Set z limits.
         min_z = np.min(np.array(data_frame['z'].values))
         max_z = np.max(np.array(data_frame['z'].values))
         height = max_z - min_z
