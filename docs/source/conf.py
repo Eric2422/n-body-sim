daf4c5ed4b7a26f0257c2bcc032b68aa6e354398
Eric Zhang <ericbkzhang@gmail.com>
2026-01-21 15:46:32 -0500

    Rewrite GH Pages deploy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ release = '0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.githubpages',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
 ]
